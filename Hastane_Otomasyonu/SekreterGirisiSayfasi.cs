53c795ec5771e2de0a61f839ea2cfcd5f0e23346
ESRA KURT <esra.kurt97833@gmail.com>
2022-01-06 23:58:21 +0300

    Sekreter Giriş Sayfasında Bulunan Giriş Yap Butonu Backendi Yapıldı.

--- Hastane_Otomasyonu/SekreterGirisiSayfasi.cs
+++ Hastane_Otomasyonu/SekreterGirisiSayfasi.cs
@@ -7,7 +7,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-
+using System.Data.Sql;
+using System.Data.SqlClient;
 namespace Hastane_Otomasyonu
 {
     public partial class SekreterGirisiSayfasi : Form
@@ -17,6 +18,7 @@ namespace Hastane_Otomasyonu
             InitializeComponent();
         }
 
+        SqlConnection baglanti = new SqlConnection("Data Source = DESKTOP-TIGD7V0; Initial Catalog = Db_Hastane; Integrated Security = True");
         private void TxtTC_TextChanged(object sender, EventArgs e)
         {
 
@@ -25,6 +27,34 @@ namespace Hastane_Otomasyonu
         private void TxtSfre_TextChanged(object sender, EventArgs e)
         {
 
+        }
+
+        private void BtnGiris_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                baglanti.Open();
+                String Sql = "select*from tbl_sekreter where SekereterTCNo=@ SekereterTCNo AND  SekereterSifre=@ SekereterSifre";
+                SqlParameter prm1 = new SqlParameter(" SekereterTCNo", TxtTC.Text.Trim());
+                SqlParameter prm2 = new SqlParameter(" SekereterSifre", TxtSfre.Text.Trim());
+                SqlCommand komut = new SqlCommand(Sql, baglanti);
+                komut.Parameters.Add(prm1);
+                komut.Parameters.Add(prm2);
+                DataTable dt = new DataTable();
+                SqlDataAdapter da = new SqlDataAdapter(komut);
+                da.Fill(dt);
+                //if (dt.Rows.Count > 0)
+                //{
+                //    FrmSekereterDetay frm = new FrmSekereterDetay();
+                //    frm.Show();
+                //    this.Hide();
+
+                //}
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Hatalı Giriş.....");
+            }
         }
     }
 }
