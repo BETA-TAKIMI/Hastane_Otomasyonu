410bb4ddb64e05ce6d73afa1590b6f260bc3d7cd
SERAP <ercelserap@gmail.com>
2022-01-07 14:28:31 +0300

    Sekreter Kayıt Ol Sayfası Tasarlandı

---
+++ Hastane_Otomasyonu/SekreterKayıtOlSayfasi.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Hastane_Otomasyonu
+{
+    public partial class SekreterKayıtOlSayfasi : Form
+    {
+        public SekreterKayıtOlSayfasi()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnKaydiTamamla_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void BtnDoktorGirisi_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
