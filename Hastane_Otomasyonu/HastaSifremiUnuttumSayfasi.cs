180f22b39ad702924ae61f27febeabff22d2bcdd
ESRA KURT <esra.kurt97833@gmail.com>
2022-01-09 00:08:25 +0300

    Hasta Şifremi Unuttum Sayfası Güncelle Butonu Backendi Yapıldı

--- Hastane_Otomasyonu/HastaSifremiUnuttumSayfasi.cs
+++ Hastane_Otomasyonu/HastaSifremiUnuttumSayfasi.cs
@@ -7,7 +7,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-
+using System.Data.Sql;
+using System.Data.SqlClient;
 namespace Hastane_Otomasyonu
 {
     public partial class HastaSifremiUnuttumSayfasi : Form
@@ -16,10 +17,26 @@ namespace Hastane_Otomasyonu
         {
             InitializeComponent();
         }
+        SqlConnection baglanti = new SqlConnection("Data Source = DESKTOP-TIGD7V0; Initial Catalog = Db_Hastane; Integrated Security = True");
 
         private void BtnGuncelle_Click(object sender, EventArgs e)
         {
+            if (TxtTC.Text == "" || TxtSfre.Text == "")
+            {
 
+                MessageBox.Show("Hatalı Giriş.....");
+            }
+            else
+            {
+                baglanti.Open();
+                SqlCommand komut = new SqlCommand("update tbl_hastalar set HastaTCNo='" + TxtTC.Text + "',HastaSifre='" + TxtSfre.Text + "' where HastaTCNo='" + TxtTC.Text + "'", baglanti);
+                komut.ExecuteNonQuery();
+                MessageBox.Show("Şifre Güncellendi...");
+
+                baglanti.Close();
+
+
+            }
         }
     }
 }
