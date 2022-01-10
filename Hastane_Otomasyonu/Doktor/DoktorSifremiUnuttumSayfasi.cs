15683bce74628b6fd9ebdc34d43456097a707532
ESRA KURT <esra.kurt97833@gmail.com>
2022-01-10 22:02:41 +0300

    Doktor Şifremi Unuttum Sayfası Güncelle Butonu Backendi Yapıldı.

--- Hastane_Otomasyonu/Doktor/DoktorSifremiUnuttumSayfasi.cs
+++ Hastane_Otomasyonu/Doktor/DoktorSifremiUnuttumSayfasi.cs
@@ -7,7 +7,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-
+using System.Data.Sql;
+using System.Data.SqlClient;
 namespace Hastane_Otomasyonu
 {
     public partial class DoktorSifremiUnuttumSayfasi : Form
@@ -16,10 +17,29 @@ namespace Hastane_Otomasyonu
         {
             InitializeComponent();
         }
+        SqlConnection baglanti = new SqlConnection("Data Source = DESKTOP-TIGD7V0; Initial Catalog = Db_Hastane; Integrated Security = True");
 
         private void BtnGuncelle_Click(object sender, EventArgs e)
         {
+            if (TxtTC.Text == "" || TxtSfre.Text == "")
+            {
 
+                MessageBox.Show("TC ve Şifre Boş Bırakılamaz!.....");
+            }
+            else
+            {
+                baglanti.Open();
+                SqlCommand komut = new SqlCommand("update tbl_doktorlar set DoktorTCNo='" + TxtTC.Text + "',DoktorSifre='" + TxtSfre.Text + "' where DoktorTCNo='" + TxtTC.Text + "'", baglanti);
+                komut.ExecuteNonQuery();
+                MessageBox.Show("Şifre Güncellendi...");
+                DoktorGirisiSayfasi hst = new DoktorGirisiSayfasi();
+                hst.Show();
+                this.Hide();
+
+                baglanti.Close();
+
+
+            }
         }
 
         private void BtnVazgec_Click(object sender, EventArgs e)
