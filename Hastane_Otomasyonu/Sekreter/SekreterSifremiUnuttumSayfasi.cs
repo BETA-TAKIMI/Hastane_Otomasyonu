492a726a21ada158f18b4ce899f3d017a3c11e90
SERAP <ercelserap@gmail.com>
2022-01-12 23:28:38 +0300

    Sekreter Şifremi Unuttum Sayfası Frontend Tamamlandı

---
+++ Hastane_Otomasyonu/Sekreter/SekreterSifremiUnuttumSayfasi.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Hastane_Otomasyonu
+{
+    public partial class SekreterSifremiUnuttumSayfasi : Form
+    {
+        public SekreterSifremiUnuttumSayfasi()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnGuncelle_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void BtnVazgec_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
