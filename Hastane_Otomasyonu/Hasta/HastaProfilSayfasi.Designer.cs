cbcbbcebf9510bcb9bda6244c0de24d8f1bb3fa4
SERAP <ercelserap@gmail.com>
2022-01-19 23:57:40 +0300

    Hasta Profil Sayfası Frontend Tamamlandı

---
+++ Hastane_Otomasyonu/Hasta/HastaProfilSayfasi.Designer.cs
@@ -0,0 +1,307 @@
+﻿namespace Hastane_Otomasyonu
+{
+    partial class HastaProfilSayfasi
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(HastaProfilSayfasi));
+            this.hesKoduLabel = new System.Windows.Forms.Label();
+            this.TxtSfre = new System.Windows.Forms.TextBox();
+            this.TxtHesKodu = new System.Windows.Forms.TextBox();
+            this.yasLabel = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.cinsiyetLabel = new System.Windows.Forms.Label();
+            this.TxtTelefon = new System.Windows.Forms.TextBox();
+            this.TxtCinsiyet = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.TxtMail = new System.Windows.Forms.TextBox();
+            this.mailLabel = new System.Windows.Forms.Label();
+            this.BtnGuncelle = new System.Windows.Forms.Button();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.pictureBox5 = new System.Windows.Forms.PictureBox();
+            this.pictureBox6 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // hesKoduLabel
+            // 
+            this.hesKoduLabel.AutoSize = true;
+            this.hesKoduLabel.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.hesKoduLabel.Location = new System.Drawing.Point(150, 93);
+            this.hesKoduLabel.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.hesKoduLabel.Name = "hesKoduLabel";
+            this.hesKoduLabel.Size = new System.Drawing.Size(79, 19);
+            this.hesKoduLabel.TabIndex = 34;
+            this.hesKoduLabel.Text = "HES Kodu";
+            // 
+            // TxtSfre
+            // 
+            this.TxtSfre.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TxtSfre.Location = new System.Drawing.Point(153, 173);
+            this.TxtSfre.Margin = new System.Windows.Forms.Padding(2);
+            this.TxtSfre.Name = "TxtSfre";
+            this.TxtSfre.Size = new System.Drawing.Size(284, 27);
+            this.TxtSfre.TabIndex = 33;
+            // 
+            // TxtHesKodu
+            // 
+            this.TxtHesKodu.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TxtHesKodu.Location = new System.Drawing.Point(153, 114);
+            this.TxtHesKodu.Margin = new System.Windows.Forms.Padding(2);
+            this.TxtHesKodu.Name = "TxtHesKodu";
+            this.TxtHesKodu.Size = new System.Drawing.Size(284, 27);
+            this.TxtHesKodu.TabIndex = 32;
+            // 
+            // yasLabel
+            // 
+            this.yasLabel.AutoSize = true;
+            this.yasLabel.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.yasLabel.Location = new System.Drawing.Point(150, 152);
+            this.yasLabel.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.yasLabel.Name = "yasLabel";
+            this.yasLabel.Size = new System.Drawing.Size(34, 19);
+            this.yasLabel.TabIndex = 31;
+            this.yasLabel.Text = "Yaş";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(266, 11);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(2);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(47, 48);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.pictureBox1.TabIndex = 35;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(234, 65);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(111, 19);
+            this.label1.TabIndex = 36;
+            this.label1.Text = "İsim Soyisim";
+            // 
+            // cinsiyetLabel
+            // 
+            this.cinsiyetLabel.AutoSize = true;
+            this.cinsiyetLabel.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cinsiyetLabel.Location = new System.Drawing.Point(150, 208);
+            this.cinsiyetLabel.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.cinsiyetLabel.Name = "cinsiyetLabel";
+            this.cinsiyetLabel.Size = new System.Drawing.Size(64, 19);
+            this.cinsiyetLabel.TabIndex = 40;
+            this.cinsiyetLabel.Text = "Cinsiyet";
+            // 
+            // TxtTelefon
+            // 
+            this.TxtTelefon.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TxtTelefon.Location = new System.Drawing.Point(153, 288);
+            this.TxtTelefon.Margin = new System.Windows.Forms.Padding(2);
+            this.TxtTelefon.Name = "TxtTelefon";
+            this.TxtTelefon.Size = new System.Drawing.Size(284, 27);
+            this.TxtTelefon.TabIndex = 39;
+            // 
+            // TxtCinsiyet
+            // 
+            this.TxtCinsiyet.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TxtCinsiyet.Location = new System.Drawing.Point(153, 229);
+            this.TxtCinsiyet.Margin = new System.Windows.Forms.Padding(2);
+            this.TxtCinsiyet.Name = "TxtCinsiyet";
+            this.TxtCinsiyet.Size = new System.Drawing.Size(284, 27);
+            this.TxtCinsiyet.TabIndex = 38;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(150, 267);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(62, 19);
+            this.label3.TabIndex = 37;
+            this.label3.Text = "Telefon";
+            // 
+            // TxtMail
+            // 
+            this.TxtMail.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TxtMail.Location = new System.Drawing.Point(153, 347);
+            this.TxtMail.Margin = new System.Windows.Forms.Padding(2);
+            this.TxtMail.Name = "TxtMail";
+            this.TxtMail.Size = new System.Drawing.Size(284, 27);
+            this.TxtMail.TabIndex = 42;
+            // 
+            // mailLabel
+            // 
+            this.mailLabel.AutoSize = true;
+            this.mailLabel.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.mailLabel.Location = new System.Drawing.Point(150, 326);
+            this.mailLabel.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.mailLabel.Name = "mailLabel";
+            this.mailLabel.Size = new System.Drawing.Size(37, 19);
+            this.mailLabel.TabIndex = 41;
+            this.mailLabel.Text = "Mail";
+            // 
+            // BtnGuncelle
+            // 
+            this.BtnGuncelle.BackColor = System.Drawing.Color.DodgerBlue;
+            this.BtnGuncelle.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BtnGuncelle.ForeColor = System.Drawing.SystemColors.InactiveBorder;
+            this.BtnGuncelle.Location = new System.Drawing.Point(154, 402);
+            this.BtnGuncelle.Margin = new System.Windows.Forms.Padding(2);
+            this.BtnGuncelle.Name = "BtnGuncelle";
+            this.BtnGuncelle.Size = new System.Drawing.Size(284, 40);
+            this.BtnGuncelle.TabIndex = 43;
+            this.BtnGuncelle.Text = "Güncelle";
+            this.BtnGuncelle.UseVisualStyleBackColor = false;
+            this.BtnGuncelle.Click += new System.EventHandler(this.BtnGuncelle_Click);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+            this.pictureBox2.Location = new System.Drawing.Point(99, 93);
+            this.pictureBox2.Margin = new System.Windows.Forms.Padding(2);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(47, 48);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.pictureBox2.TabIndex = 44;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox3.Image")));
+            this.pictureBox3.Location = new System.Drawing.Point(99, 152);
+            this.pictureBox3.Margin = new System.Windows.Forms.Padding(2);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(47, 48);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.pictureBox3.TabIndex = 45;
+            this.pictureBox3.TabStop = false;
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox4.Image")));
+            this.pictureBox4.Location = new System.Drawing.Point(99, 208);
+            this.pictureBox4.Margin = new System.Windows.Forms.Padding(2);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(47, 48);
+            this.pictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.pictureBox4.TabIndex = 46;
+            this.pictureBox4.TabStop = false;
+            // 
+            // pictureBox5
+            // 
+            this.pictureBox5.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox5.Image")));
+            this.pictureBox5.Location = new System.Drawing.Point(99, 267);
+            this.pictureBox5.Margin = new System.Windows.Forms.Padding(2);
+            this.pictureBox5.Name = "pictureBox5";
+            this.pictureBox5.Size = new System.Drawing.Size(47, 48);
+            this.pictureBox5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.pictureBox5.TabIndex = 47;
+            this.pictureBox5.TabStop = false;
+            // 
+            // pictureBox6
+            // 
+            this.pictureBox6.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox6.Image")));
+            this.pictureBox6.Location = new System.Drawing.Point(99, 326);
+            this.pictureBox6.Margin = new System.Windows.Forms.Padding(2);
+            this.pictureBox6.Name = "pictureBox6";
+            this.pictureBox6.Size = new System.Drawing.Size(47, 48);
+            this.pictureBox6.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.pictureBox6.TabIndex = 48;
+            this.pictureBox6.TabStop = false;
+            // 
+            // HastaProfilSayfasi
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.LightBlue;
+            this.ClientSize = new System.Drawing.Size(580, 453);
+            this.Controls.Add(this.pictureBox6);
+            this.Controls.Add(this.pictureBox5);
+            this.Controls.Add(this.pictureBox4);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.BtnGuncelle);
+            this.Controls.Add(this.TxtMail);
+            this.Controls.Add(this.mailLabel);
+            this.Controls.Add(this.cinsiyetLabel);
+            this.Controls.Add(this.TxtTelefon);
+            this.Controls.Add(this.TxtCinsiyet);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.hesKoduLabel);
+            this.Controls.Add(this.TxtSfre);
+            this.Controls.Add(this.TxtHesKodu);
+            this.Controls.Add(this.yasLabel);
+            this.Name = "HastaProfilSayfasi";
+            this.Text = "Hasta Profil Sayfası";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label hesKoduLabel;
+        private System.Windows.Forms.TextBox TxtSfre;
+        private System.Windows.Forms.TextBox TxtHesKodu;
+        private System.Windows.Forms.Label yasLabel;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label cinsiyetLabel;
+        private System.Windows.Forms.TextBox TxtTelefon;
+        private System.Windows.Forms.TextBox TxtCinsiyet;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox TxtMail;
+        private System.Windows.Forms.Label mailLabel;
+        private System.Windows.Forms.Button BtnGuncelle;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.PictureBox pictureBox5;
+        private System.Windows.Forms.PictureBox pictureBox6;
+    }
+}
